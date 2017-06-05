7419519386162a4ec313367ed95d2634fcb48dd0
kfrn <knfrances@gmail.com>
2017-06-05 23:15:40 +1200

    CSS now working with dynamic routing yaaaaay
    • The fix: static assets need to be prefaced with '\'.
    • See https://github.com/ReactTraining/react-router/issues/2213#issuecomment-146847612

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
     <div className="container">
       <h3>Error 404</h3>
       <p className="error">Page not found :(</p>
-      <img src='./images/Murakami_MicahLidberg_AfterDark.jpg' alt="Murakami-inspired illustration by Micah Lidberg"></img>
+      <img src='/images/Murakami_MicahLidberg_AfterDark.jpg' alt="Murakami-inspired illustration by Micah Lidberg"></img>
       <p className='attribution'>Artwork by <a href='http://www.micahlidberg.com/'>Micah Lidberg</a></p>
     </div>
   );
