f3211bfa9fcaf5a3511e24fe2d4e4e4ffdb24c40
kfrn <knfrances@gmail.com>
2017-07-23 18:50:00 +1200

    Fix about text

--- src/components/About.js
+++ src/components/About.js
@@ -10,38 +10,13 @@ const About = () => {
         alt="Portrait of Haruki Murakami"
       />
       <p>
-        Haruki Murakami (村上 春樹 Murakami Haruki, January 12, 1949) is a Japanese
-        writer. His books and stories have been bestsellers in Japan as well as
-        internationally, with his work being translated into 50 languages and
-        selling millions of copies outside his native country. The critical
-        acclaim for his fiction and non-fiction has led to numerous awards, in
-        Japan and internationally, including the World Fantasy Award (2006) and
-        the Frank O'Connor International Short Story Award (2006). His oeuvre
-        received, for example, the Franz Kafka Prize (2006) and the Jerusalem
-        Prize (2009).
+        Haruki Murakami (村上 春樹 Murakami Haruki, January 12, 1949) is a Japanese writer. His books and stories have been bestsellers in Japan as well as internationally, with his work being translated into 50 languages and selling millions of copies outside his native country. The critical acclaim for his fiction and non-fiction has led to numerous awards, in Japan and internationally, including the World Fantasy Award (2006) and the Frank O'Connor International Short Story Award (2006). His oeuvre received, for example, the Franz Kafka Prize (2006) and the Jerusalem Prize (2009).
       </p>
       <p>
-        In 1978 Murakami was in the bleachers of Jingu Stadium watching a
-        baseball game between the Yakult Swallows and the Hiroshima Carp when
-        Dave Hilton, an American, came to bat. According to an oft-repeated
-        story, in the instant that he hit a double, Murakami suddenly realized
-        that he could write a novel. He went home and began writing that night.
+        In 1978 Murakami was in the bleachers of Jingu Stadium watching a baseball game between the Yakult Swallows and the Hiroshima Carp when Dave Hilton, an American, came to bat. According to an oft-repeated story, in the instant that he hit a double, Murakami suddenly realized that he could write a novel. He went home and began writing that night.
       </p>
       <p>
-        Murakami's most notable works include <em>A Wild Sheep Chase</em>
-        (1982), <em>Norwegian Wood</em> (1987),
-        <em>The Wind-Up Bird Chronicle</em> (1994–95),
-        <em>Kafka on the Shore</em> (2002), and <em>1Q84</em> (2009–10). He has
-        also translated into Japanese English works by writers ranging from
-        Raymond Carver to J. D. Salinger. His fiction, still criticized by
-        Japan's literary establishment as un-Japanese, was influenced by Western
-        writers from Chandler to Vonnegut by way of Brautigan. It is frequently
-        surrealistic and melancholic or fatalistic, marked by a Kafkaesque
-        rendition of the "recurrent themes of alienation and loneliness" he
-        weaves into his narratives. He is also considered an important figure in
-        postmodern literature. Steven Poole of The Guardian praised Murakami as
-        "among the world's greatest living novelists" for his works and
-        achievements.
+        Murakami's most notable works include <em>A Wild Sheep Chase</em> (1982), <em>Norwegian Wood</em> (1987), <em>The Wind-Up Bird Chronicle</em> (1994–95), <em>Kafka on the Shore</em> (2002), and <em>1Q84</em> (2009–10). He has also translated into Japanese English works by writers ranging from Raymond Carver to J. D. Salinger. His fiction, still criticized by Japan's literary establishment as un-Japanese, was influenced by Western writers from Chandler to Vonnegut by way of Brautigan. It is frequently surrealistic and melancholic or fatalistic, marked by a Kafkaesque rendition of the "recurrent themes of alienation and loneliness" he weaves into his narratives. He is also considered an important figure in postmodern literature. Steven Poole of The Guardian praised Murakami as "among the world's greatest living novelists" for his works and achievements.
       </p>
     </div>
   );
